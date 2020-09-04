99350ad2c4235119660ff51afe63b323518779bc
Kenny Lazer <kennyruzindana10@gmail.com>
2020-09-04 17:41:19 +0200

    Deployed admin credentials on heroku

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -7,7 +7,7 @@ class adminController {
     const { username, password } = req.body;
     if (username !== config.ADMIN_USERNAME || password !== config.ADMIN_PASSWORD) return out(res, 400, 'Username or Password is incorrect', null, 'AUTHENTICATION_ERROR');
     const token = await sign({ username, password, role: 'admin' });
-    return out(res, 200, 'Loggedd in successfully', { username, role: 'admin', token });
+    return out(res, 200, 'Logged in successfully', { username, role: 'admin', token });
   }
 }
 
