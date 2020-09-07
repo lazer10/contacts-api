ae009a7561dd8fce47d896aaf91702a5b5e1d7c7
Kenny Lazer <kennyruzindana10@gmail.com>
2020-09-07 21:19:18 +0200

    Reasses admin login

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -4,10 +4,14 @@ import config from '../config';
 
 class adminController {
   static async login(req, res) {
-    const { username, password } = req.body;
-    if (username !== config.ADMIN_USERNAME || password !== config.ADMIN_PASSWORD) return out(res, 400, 'Username or Password is incorrect', null, 'AUTHENTICATION_ERROR');
-    const token = await sign({ username, password, role: 'admin' });
-    return out(res, 200, 'Logged in successfully', { username, role: 'admin', token });
+    try {
+      const { username, password } = req.body;
+      if (username !== config.ADMIN_USERNAME || password !== config.ADMIN_PASSWORD) return out(res, 400, 'Username or Password is incorrect', null, 'AUTHENTICATION_ERROR');
+      const token = await sign({ username, password, role: 'admin' });
+      return out(res, 200, 'Logged in successfully great', { username, role: 'admin', token });
+    } catch (error) {
+      return out(res, 500, error.message || error, null, 'SERVER_ERROR');
+    }
   }
 }
 
